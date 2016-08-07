45c7f4f2fb8ab70e46c228c3c3d5158e167b125b
myflwq <1084091692@qq.com>
2016-08-07 10:25:33 +0800

    list-loading

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
     }
     return(
       <div >
-        <div style={{backgroundColor:'rgb(0, 188, 212)',height:'68px'}}>
+        <div style={{backgroundColor:'rgb(0, 188, 212)',height:'10vh'}}>
           <IconButton tooltip="menu" style={styles.icon} iconStyle={styles.svg} onClick={this.handelOpen.bind(this)}>
             <MenuButton  color='#fff'/>
           </IconButton>
--- src/component/Blog/List/List.js
+++ src/component/Blog/List/List.js
@@ -1,51 +1,64 @@
 import React, { Component } from 'react';
 import map from 'lodash/fp/map';
-import BlogCard from './BlogCard.js';
 import axios from 'axios';
-
+import CircularProgress from 'material-ui/CircularProgress';
+import BlogCard from './BlogCard.js';
 
 class List extends Component {
+
   constructor(){
     super();
     this.state={
-      posts: ''
+      posts: '',
+      wait:true
     }
   }
   componentDidMount() {
     axios.get('https://raw.githubusercontent.com/myflwq/react-router/master/posts/index.json')
     .then((res) => {
        this.setState({
-         posts: res.data
+         posts: res.data,
+         wait:false
        });
     });
   }
-  render(){
+  getStyles() {
+     return {
+       loading:{
 
+         margin:'0 auto',width:'130px',
+         height:'200px'
+       }
+     }
+   }
+  render(){
+    const styles = this.getStyles();
+    let x = <div style={styles.loading}><p>正在获取数据......</p><CircularProgress size={1.5} /></div>;
   // console.log(Cards.length);
   // console.log(this.props.search);
-  let posts = this.state.posts
-  console.log(posts);
-  var blogCards = [];
-    if (this.props.search=='') {
-      map((b) =>  {
-                  blogCards.push(
-                    <BlogCard title={b.title} date={b.created_at } index={b.index} key={Math.random()}/>
-                  );
-                },
-        posts
-      );
-    }else {
-        let query=new RegExp(this.props.search,"i");
-        for (var key in posts) {
-          if (query.test(posts[key].title)) {
-            console.log("aaaa");
-            blogCards.push(<BlogCard title={posts[key].title} date={posts[key].date} index={posts[key].index} key={Math.random()}/>)
+    let posts = this.state.posts
+    // console.log(posts);
+    var blogCards = [];
+      if (this.props.search=='') {
+        map((b) =>  {
+                    blogCards.push(
+                      <BlogCard title={b.title} date={b.created_at } index={b.index} key={Math.random()}/>
+                    );
+                  },
+          posts
+        );
+      }else {
+          let query=new RegExp(this.props.search,"i");
+          for (var key in posts) {
+            if (query.test(posts[key].title)) {
+              console.log("aaaa");
+              blogCards.push(<BlogCard title={posts[key].title} date={posts[key].date} index={posts[key].index} key={Math.random()}/>)
+            }
           }
         }
-      }
     return(
       <div>
-        {blogCards}
+        {this.state.wait ? x : blogCards}
       </div>
     )
   }
